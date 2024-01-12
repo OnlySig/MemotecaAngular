347f3ae60c06a73a28b39b465742c31397b5f90e
OnlySig <gabriel.castro.pereira11@gmail.com>
2024-01-12 04:04:52 -0300

    pratica: diretivas como ngFor + ngIf + ngClass

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css']
 })
 export class ListarPensamentoComponent {
+  listaPensamentos = [
+    {
+      conteudo: 'I LOVE CLAIDE',
+      autoria: 'CarlinhosS',
+      modelo: 'modelo3'
+    },
 
+    {
+      conteudo: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et neque iure hic obcaecati accusantium expedita tenetur vel est ut quis placeat, sunt dolores odio ducimus rem exercitationem recusandae culpa inventore. xdddddddddddddddddddddddddddddddddddddddddddddddddd',
+      autoria: 'Carlinhos',
+      modelo: 'modelo1'
+    }
+  ]
 }
+
